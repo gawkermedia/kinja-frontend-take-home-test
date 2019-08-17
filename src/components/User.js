9d4485e74f393ca9425de468589314c51462063c
Csaba Schreiner <ed@kinja.com>
2019-08-17 10:06:16 +0200

    feat(*): add styles (#1)

--- src/components/User.js
+++ src/components/User.js
@@ -5,8 +5,27 @@ import styled from 'styled-components'
 import type { Kinja$User } from '../api/profile'
 
 const UserContainer = styled.details`
-    border-bottom: 1px solid #fafafa;
-    padding: 10px;
+    border-bottom: 1px solid #ccc;
+
+    summary {
+        padding: 15px;
+        outline: none;
+
+        :hover {
+            background-color: #f1f1f1;
+        }
+    }
+`
+
+const DataGrid = styled.div`
+    display: grid;
+    grid-template-columns: 60px 1fr;
+    grid-gap: 15px;
+    padding: 0 15px 15px;
+`
+
+const DataGridLabel = styled.div`
+    color: #666;
 `
 
 type Props = Kinja$User
@@ -15,10 +34,10 @@ export function UserItem({ name, email, role }: Props) {
     return (
         <UserContainer key={name}>
             <summary>{name}</summary>
-            <ul>
-                <li>email: {email}</li>
-                <li>role: {role}</li>
-            </ul>
+            <DataGrid>
+                <DataGridLabel>E-mail:</DataGridLabel><div>{email}</div>
+                <DataGridLabel>Role:</DataGridLabel><div>{role}</div>
+            </DataGrid>
         </UserContainer>
     )
 }
